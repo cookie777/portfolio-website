79a8555ed5cc911edc6ca019a8f3454730bd833d
cookie777 <l.oeil.ballon@gmail.com>
2021-05-07 16:02:13 -0700

    feat: add sass

--- pages/test-post.js
+++ pages/test-post.js
@@ -1,3 +1,25 @@
+import { getSinglePostDataOf } from '../lib/posts'
+import styles from '../styles/works.module.scss'
+
+export default function TestPage({ contentHtml }) {
+
+  return (
+    <div className={styles.test}>
+      <div dangerouslySetInnerHTML={{ __html: contentHtml}} />
+    </div>
+      
+  )
+}
+
+export async function getStaticProps() {
+  const {contentHtml} =  await getSinglePostDataOf('works', `project1`)
+  return {
+    props: {
+      contentHtml
+    }
+  }
+}
+
 // import fs from 'fs'
 // import path from 'path'
 // import { serialize } from 'next-mdx-remote/serialize'
@@ -73,38 +95,38 @@
 // }
 
 
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 
-import ReactMarkdown from "react-markdown";
-import styled from 'styled-components';
+// import ReactMarkdown from "react-markdown";
+// import styled from 'styled-components';
 
-const StyledP = styled.p`
-font-size: 40px;
-color: green;
-`;
+// const StyledP = styled.p`
+// font-size: 40px;
+// color: green;
+// `;
 
-const components = {
-  p: StyledP
-}
+// const components = {
+//   p: StyledP
+// }
 
-export default function TestPage({ source }) {
+// export default function TestPage({ source }) {
 
-  return (
-    <div>
-      <ReactMarkdown children={source} components={components}/>
-    </div>
+//   return (
+//     <div>
+//       <ReactMarkdown children={source} components={components}/>
+//     </div>
       
-  )
-}
+//   )
+// }
 
-export async function getStaticProps() {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const fullPath = path.join(postsDirectory + '/' + `works`, `project1.md`)
-  const source = fs.readFileSync(fullPath, 'utf8')
+// export async function getStaticProps() {
+//   const postsDirectory = path.join(process.cwd(), 'posts')
+//   const fullPath = path.join(postsDirectory + '/' + `works`, `project1.md`)
+//   const source = fs.readFileSync(fullPath, 'utf8')
 
-  return { props: { source } }
-}
+//   return { props: { source } }
+// }
 
 
 
--- pages/works/[id].js
+++ pages/works/[id].js
@@ -1,63 +1,18 @@
-// import { getAllPostIdsFrom, getSinglePostDataOf } from '../../lib/posts'
+import { getAllPostIdsFrom, getSinglePostDataOf } from '../../lib/posts'
 
-// import styles from '../../styles/works.module.scss'
-
-// export default function Works({postData}) {
-
-//   return (
-//     <div className={styles.test}>
-//       {postData.title}
-//       <br />
-//       {postData.id}
-//       <br />
-//       {postData.date}
-//       <br />
-//       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-//       <p>test</p>
-//     </div>
-//   )
-// }
-
-// export async function getStaticPaths() {
-//   const paths = getAllPostIdsFrom('works')
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
-
-// export async function getStaticProps({ params }) {
-//   const postData =  await getSinglePostDataOf('works', params.id)
-//   return {
-//     props: {
-//       postData
-//     }
-//   }
-// }
-
-
-
-import fs from 'fs'
-import path from 'path'
-import { getAllPostIdsFrom } from '../../lib/posts'
-
-import ReactMarkdown from "react-markdown";
-import styled from 'styled-components';
-
-const StyledP = styled.p`
-font-size: 40px;
-color: green;
-`;
-
-const components = {
-  p: StyledP
-}
+import styles from '../../styles/works.module.scss'
 
 export default function Works({postData}) {
 
   return (
-    <div>
-      <ReactMarkdown children={postData} components={components}/>
+    <div className={styles.test}>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       <p>test</p>
     </div>
   )
@@ -72,12 +27,57 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const fullPath = path.join(postsDirectory + '/' + `works`, `${params.id}.md`)
-  const postData = fs.readFileSync(fullPath, 'utf8')
+  const postData =  await getSinglePostDataOf('works', params.id)
   return {
     props: {
       postData
     }
   }
 }
+
+
+
+// import fs from 'fs'
+// import path from 'path'
+// import { getAllPostIdsFrom } from '../../lib/posts'
+
+// import ReactMarkdown from "react-markdown";
+// import styled from 'styled-components';
+
+// const StyledP = styled.p`
+// font-size: 40px;
+// color: green;
+// `;
+
+// const components = {
+//   p: StyledP
+// }
+
+// export default function Works({postData}) {
+
+//   return (
+//     <div>
+//       <ReactMarkdown children={postData} components={components}/>
+//       <p>test</p>
+//     </div>
+//   )
+// }
+
+// export async function getStaticPaths() {
+//   const paths = getAllPostIdsFrom('works')
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
+
+// export async function getStaticProps({ params }) {
+//   const postsDirectory = path.join(process.cwd(), 'posts')
+//   const fullPath = path.join(postsDirectory + '/' + `works`, `${params.id}.md`)
+//   const postData = fs.readFileSync(fullPath, 'utf8')
+//   return {
+//     props: {
+//       postData
+//     }
+//   }
+// }
